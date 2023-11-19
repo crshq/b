8213158165f37265a51ebccc2c26be31976dc09d
Some random dude <125770478+crshq@users.noreply.github.com>
2023-11-19 15:07:43 -0500

    auto resize

--- flash.js
+++ flash.js
@@ -1,5 +1,11 @@
 var a = document.createElement('object');
 const query = new URLSearchParams(window.location.search);
-a.classList.add('screencover');
 a.setAttribute("data", "https://crshq.github.io/b-data/" + query.get("flash"));
+a.id = "flash"
 document.body.appendChild(a);
+let b = documnt.getElementById('flash');
+let c = true
+while (c) {
+    b.setAttribute('width', window.innerWidth);
+    b.setAttribute('height', window.innerHeight);
+}
