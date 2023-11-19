87f45bc71053c0a5de886f3edd0ff3d95040d940
Some random dude <125770478+crshq@users.noreply.github.com>
2023-11-19 14:22:04 -0500

    update to ruffle standards

--- flash.js
+++ flash.js
@@ -1,7 +1,5 @@
 var a = document.createElement('object');
 const query = new URLSearchParams(window.location.search);
-a.setAttribute("type", "application/x-shockwave-flash");
-a.setAttribute("name", "thing");
-a.setAttribute("id", "thing");
-a.setAttribute("src", "https://github.com/crshq/b-data/raw/main/" + query.get("flash"));
+a.classList.add('screencover');
+a.setAttribute("data", "https://github.com/crshq/b-data/raw/main/" + query.get("flash"));
 document.body.appendChild(a);
