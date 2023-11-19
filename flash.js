a7099f8fc335cf0fea09cc6880e29ada19154980
Some random dude <125770478+crshq@users.noreply.github.com>
2023-11-19 15:27:18 -0500

    resize

--- flash.js
+++ flash.js
@@ -1,11 +1,7 @@
 var a = document.createElement('object');
 const query = new URLSearchParams(window.location.search);
 a.setAttribute("data", "https://crshq.github.io/b-data/" + query.get("flash"));
-a.id = "flash"
+a.id = "flash";
+a.setAttribute('width', window.innerWidth);
+a.setAttribute('height', window.innerHeight);
 document.body.appendChild(a);
-let b = documnt.getElementById('flash');
-let c = true
-while (c) {
-    b.setAttribute('width', window.innerWidth);
-    b.setAttribute('height', window.innerHeight);
-}
