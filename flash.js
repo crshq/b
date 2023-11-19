f67cd5871a47a3cd5d9f71a4ac3880ab61fc9062
Some random dude <125770478+crshq@users.noreply.github.com>
2023-11-19 14:36:27 -0500

    fix cors v2?

--- flash.js
+++ flash.js
@@ -1,5 +1,5 @@
 var a = document.createElement('object');
 const query = new URLSearchParams(window.location.search);
 a.classList.add('screencover');
-a.setAttribute("data", "crshq.github.io/b-data/" + query.get("flash"));
+a.setAttribute("data", "https://crshq.github.io/b-data/" + query.get("flash"));
 document.body.appendChild(a);
